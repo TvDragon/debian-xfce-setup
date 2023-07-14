3439d3a3a664596a079f8455fb033de5c17ef1d1
Tony Vo <tonyvo249@gmail.com>
2023-07-14 22:23:59 +1000

    xinitrc startup xfce

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ sudo nala install pulseaudio thunar network-manager neovim curl lightdm -y
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
 mkdir ~/.config
+cp .xinitrc ~/
 cp -r debian_test_setup/dotconfigs/* ~/.config/
