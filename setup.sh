693413e34d9cf25e4997ca13c2e45198a5333370
Tony Vo <tonyvo249@gmail.com>
2023-07-15 19:49:51 +1000

    xfce4 session

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #sudo nala install xfwm4 xfwm4-settings xfwm4-tweaks-settings xfwm4-workspace-settings xfce4-terminal xfce4-keyboard-settings -y
 cd
-sudo nala install xfwm4 xfce4-terminal xfce4-settings xfce4-taskmanager xfce4-notifyd xfce4-screenshooter ristretto xfce4-panel xfce4-panel-profiles xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin xfce4-windowck-plugin -y
+sudo nala install xfwm4 xfce4-terminal xfce4-settings xfce4-session xfce4-taskmanager xfce4-notifyd xfce4-screenshooter ristretto xfce4-panel xfce4-panel-profiles xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin xfce4-windowck-plugin -y
 sudo nala install pulseaudio thunar network-manager neovim curl lightdm -y
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
